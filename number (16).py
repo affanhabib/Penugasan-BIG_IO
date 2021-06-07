04557fe7fc7870124d4d2af62ad246cf59cd626c
Affan Ahsanul Habib <ahsanulhabib11@gmail.com>
2021-06-07 20:23:06 +0700

    Revisi no 16 dan Tambah no 17

--- number (16).py
+++ number (16).py
@@ -1,41 +1,46 @@
-x = 5
+while True:
+    # print("Masukkan Angka: ")
+    x = int(input())
 
-n = 1
-y = 2 * x
-for i in range(0, x):
-    n = 1
-    for j in range(0, i):
-        print(n, end="")
-        n = n + 1
-    for k in range(0, y-1):
-        print(end=" ")
-    y = y - 2
-    for l in range(0, i):
-        print(n-1, end="")
-        n = n - 1
-    print("\r")
+    if x == "q":
+        quit
+    else:
+        n = 1
+        y = 2 * x
+        for i in range(0, x):
+            n = 1
+            for j in range(0, i):
+                print(n, end="")
+                n = n + 1
+            for k in range(0, y-1):
+                print(end=" ")
+            y = y - 2
+            for l in range(0, i):
+                print(n-1, end="")
+                n = n - 1
+            print("\r")
 
-for i in range(0, x):
-    print(n, end="")
-    n = n + 1
-for j in range(0, x-1):
-    print(n-2, end="")
-    n = n - 1
-print("\r")
+        for i in range(0, x):
+            print(n, end="")
+            n = n + 1
+        for j in range(0, x-1):
+            print(n-2, end="")
+            n = n - 1
+        print("\r")
 
-m = x - 1
-o = x - 1
-for i in range(1, x):
-    n = 1
-    for j in range(0, m):
-        print(n, end="")
-        n = n + 1
-    m = m - 1
-    for k in range(0, y+1):
-        print(end=" ")
-    y = y + 2
-    for l in range(0, o):
-        print(n-1, end="")
-        n = n - 1
-    o = o - 1
-    print("\r")
+        m = x - 1
+        o = x - 1
+        for i in range(1, x):
+            n = 1
+            for j in range(0, m):
+                print(n, end="")
+                n = n + 1
+            m = m - 1
+            for k in range(0, y+1):
+                print(end=" ")
+            y = y + 2
+            for l in range(0, o):
+                print(n-1, end="")
+                n = n - 1
+            o = o - 1
+            print("\r")
